fd180368955938d9f1c9ae19523fdd6f3d905af2
mrx7355608 <mrx7355608@gmail.com>
2025-01-19 07:01:20 -0800

    apollo client setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const client = new ApolloClient({
+    uri: import.meta.env.VITE_SERVER_URL,
+    cache: new InMemoryCache(),
+});
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    </StrictMode>,
+);
