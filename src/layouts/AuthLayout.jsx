9c841d6c64bca866d32ee61a2df44d306ef98510
mrx7355608 <mrx7355608@gmail.com>
2025-01-20 17:50:40 -0800

    added route protection

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,8 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import AuthNavbar from "../components/navbar/AuthNavbar";
 import Footer from "../components/Footer";
+import { useAuth } from "../states/AuthProvier";
 
 export default function AuthLayout() {
+    const { user } = useAuth();
+
+    // If user is already authenticated, redirect him to home route
+    if (user) {
+        return <Navigate to={"/"} />;
+    }
+
     return (
         <div>
             <AuthNavbar />
--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,9 +1,16 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
 import { HomeNavbar } from "../components/navbar/HomeNavbar";
 import Footer from "../components/Footer";
+import { useAuth } from "../states/AuthProvier";
 
 export default function HomeLayout() {
+    const { user } = useAuth();
+
+    // If user is not authenticated, redirect him to login route
+    if (user === null) {
+        return <Navigate to={"/auth/login"} />;
+    }
+
     return (
         <div>
             <HomeNavbar />
